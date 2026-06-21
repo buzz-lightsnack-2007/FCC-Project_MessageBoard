a01b2d2d4920447f03c9c0aee2b1b785236dd9e4
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2026-06-21 14:50:10 +0000

    feat: Implement security validation and hashing

---
+++ src/backend/security/code.js
@@ -0,0 +1,126 @@
+﻿/**
+ * @file code.js
+ * The data structure and methods for security-related features
+ * 
+ * @module security/code
+ */
+// Imports
+const zod = require('zod');
+const errors = require('common-errors');
+let bcrypt = require('bcryptjs');
+
+let securityMessaging = require('./messaging');
+
+/**
+ * The configuration for hashes
+ * 
+ * This configuration will be pulled from environment variables. 
+ * 
+ * @type {dict}
+*/
+const hash_config = {
+	"native": process.env?.security_algorithms_useBcrypt || false,
+	"rounds": process.env?.security_HashRounds || 13,
+	"salt": process.env?.security_HashSalt
+};
+
+/**
+ * Configures the hashing algorithm to use. Optionally, passing a verbose flag will print out the configuration being used.
+ * 
+ * @param {Boolean} verbose - Whether to print out the configuration being used
+ * @param {String} delimiter - The delimiter to use when printing the configuration
+ */
+const config = (verbose = false, delimiter = "\n\t- ") => {
+	if (hash_config[`native`]) {
+		try {
+			bcrypt = require('bcrypt');
+		} catch (error) {
+			// If bcrypt is not available, fall back to bcryptjs
+			error.message = `bcrypt isn't available, so we're falling back to bcryptjs. ${error.message}`;
+			console.warn(error);
+		};
+	};
+
+	verbose && console.log(`Using the following authentication configuration: ${delimiter} ${Object.entries(hash_config).map(([key, value]) => `${key}: \t${value || `\e[2m(unspecified)\e[0m`}`).join(delimiter)}`);
+};
+
+config();
+
+/**
+ * @class Hash
+ * A hash of a password, with methods to create and verify hashes
+ */
+class Hash {
+	/**
+	 * The hash
+	 * @type {string}
+	 */
+	#hash;
+
+	/**
+	 * The hash
+	 * @type {string}
+	 * @readonly
+	 */
+	get hash() { return this.#hash; }
+
+	/**
+	 * @constructor
+	 * Initializes a hash object with an existing hash string. 
+	 * 
+	 * Use this if the hash was already created and was, for example, stored in a database. 
+	 * 
+	 * @param {string} hash 
+	 */
+	constructor(hash) {
+		this.#hash = hash;
+	};
+
+	/**
+	 * Generate a hash from a password string.
+	 * 
+	 * @param {string} password - the password
+	 * @param {Number} [rounds = hash_config.rounds] - the number of rounds to use for hashing
+	 * @param {string} [salt = hash_config.salt] - the salt to use for hashing
+	 * @returns {Hash} The generated hash
+	 */
+	static from(password, rounds = hash_config['rounds'], salt = hash_config['salt']) {
+		rounds = zod.number().gte(0).parse((typeof rounds == `function`) ? rounds() : rounds);
+
+		salt = (zod.string().safeParse(salt).success && salt) || bcrypt.genSaltSync(rounds);
+
+		const hash = bcrypt.hashSync(password, salt);
+		return new Hash(hash);
+	};
+
+	/**
+	 * Compare a password to the hash.
+	 * 
+	 * @method compare
+	 * @param {string} password - the password to compare
+	 * @param {boolean} [raise = false] - whether to throw an error if the password does not match
+	 * @returns {boolean} True if the password matches the hash, false otherwise
+	 */
+	compare(password, raise = false) {
+		const result = bcrypt.compareSync(password, this.#hash);
+
+		let message = (result) ? new securityMessaging.Messages.Success(password, this.#hash, this) : new securityMessaging.Messages.Failure(password, this.#hash, this);
+
+		if (raise && !result) { throw message.error; };
+		return message;
+	};
+
+	/**
+	 * Gets the salt portion from the hash. 
+	 * 
+	 * @returns {string} The salt portion of the hash
+	 */
+	get salt() {
+		return bcrypt.getSalt(this.#hash);
+	}
+};
+
+module.exports = {
+	Hash,
+	hash_config
+};
---
+++ src/backend/security/messaging.js
@@ -0,0 +1,118 @@
+﻿/**
+ * Security-related messaging
+ * 
+ * @file messaging.js
+ * @module security/messaging
+ */
+
+// Imports
+const errors = require('common-errors');
+const zod = require('zod');
+
+const messaging = require('../utils/messaging');
+
+const Errors = {
+	"AuthenticationError":
+		class AuthenticationError extends errors.AuthenticationRequiredError {
+			/**
+			 * The password used
+			 * @type {string}
+			 */
+			password;
+
+			/**
+			 * The hash of the entity being authenticated
+			 * @type {import('./code').Hash}
+			 */
+			hash;
+
+			/**
+			 * @constructor
+			 * @param {String} password - The password used
+			 * @param {import('./code').Hash} hash - The hash of the entity being authenticated
+			 * @param {String} message - any message
+			 * @param {Error} inner_error - the Error instance that caused the current error. Stack trace will be appended. 
+			 * @see {@link https://www.npmjs.com/package/common-errors#authrequired}
+			 */
+			constructor(password, hash, ...arguments) {
+				super(...arguments);
+				this.password = password;
+				this.hash = hash;
+			};
+		}
+}
+
+const Messages = {
+	"Success":
+		/**
+		 * @class AuthenticationSuccess
+		 * 
+		 * Indicates an authentication request was successful
+		 */
+		class AuthenticationSuccess extends messaging.Success {
+			/**
+			 * The password of the authenticated entity
+			 * @type {string}
+			 */
+			password;
+
+			/**
+			 * The hash of the authenticated entity
+			 * @type {import('./code').Hash}
+			 */
+			result;
+
+			/**
+			 * @constructor
+			 * @param {string} password - The password of the authenticated entity
+			 * @param {import('./code').Hash} hash - The hash of the authenticated entity
+			 * @param {Object} cause - the process that has determined a success result
+			 * @param {String} description - a description of the message
+			 */
+			constructor(password, hash, cause, description = ``) {
+				super(hash, cause, description);
+				this.password = password;
+			};
+		},
+	"Failure":
+		/**
+		 * @class AuthenticationFailure
+		 * 
+		 * Indicates an authentication request was unsuccessful
+		 */
+		class AuthenticationFailure extends messaging.Error {
+			/**
+			 * The error details
+			 * @type {Errors['AuthenticationError']}
+			*/
+			error;
+
+			/**
+			 * The password used
+			 * @type {string}
+			 */
+			get password() { return this.error?.password; }
+
+			/**
+			 * The hash of the entity being authenticated
+			 * @type {import('./code').Hash}
+			 */
+			get hash() { return this.error?.hash; }
+
+			/**
+			 * @constructor
+			 * @param {string} password - The password used
+			 * @param {import('./code').Hash} hash - The hash of the entity being authenticated
+			 * @param {Object} cause - the process that has determined a failure result
+			 * @param {String} description - a description of the message
+			 */
+			constructor(password, hash, cause = undefined, description = undefined) {
+				super(new Errors.AuthenticationError(password, hash), cause, description);
+			};
+		}
+};
+
+module.exports = {
+	Errors,
+	Messages
+};
