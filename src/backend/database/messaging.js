2996df959768d3254314f1029e5d1d68a84db494
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2026-06-24 14:49:45 +0800

    fix: refactor error messages using `CustomError`

--- src/backend/database/messaging.js
+++ src/backend/database/messaging.js
@@ -4,13 +4,58 @@
  * @module database/messaging
  */
 
+const errors = require(`../utils/messaging`)
+
 /**
- * @require common-errors
+ * When a filter yields no results
+ * @class NotFoundError
+ * @extends InternalError
  */
-const errors = require(`common-errors`);
+class NotFoundError extends errors.InternalError {
+	/**
+	 * The search query
+	 * @type {Object}
+	 */
+	filter; 
 
-const Errors = {
-	NotFoundError: errors.NotFoundError
-}
+	/**
+	 * @constructor
+	 * 
+	 * Constructs a new `NotFoundError` instance. 
+	 * 
+	 * @param {Object} filter - the search query
+	 * @param {String} message - any message
+	 */
+	constructor(filter, message) {
+		super(message, 404);
+		this.filter = filter;
+	}; 
+}; 
 
-module.exports = {Errors};
+/**
+ * When an entity is not found
+ * @class EntityNotFoundError
+ * @extends NotFoundError
+ */
+class EntityNotFoundError extends NotFoundError {
+	/**
+	 * The ID of the entity being searched for
+	 * @type {String|Number}
+	 */
+	filter;
+
+	/**
+	 * @constructor
+	 * 
+	 * Constructs a new `EntityNotFoundError` instance.
+	 * 
+	 * @param {String|Number} ID - the ID of the entity being searched for
+	 * @param {String} message - any message
+	 */
+	constructor(ID, message) {
+		super(ID, message);
+		this.filter = ID;
+	}; 
+}; 
+
+module.exports = {NotFoundError, EntityNotFoundError};
---
+++ src/backend/security/errors.js
@@ -0,0 +1,50 @@
+﻿const InternalError = require(`../utils/messaging`).InternalError; 
+const zod = require('zod');
+
+/**
+ * When authentication is required but not provided
+ * @class AuthenticationRequiredError
+ * @extends InternalError
+ */
+class AuthenticationRequiredError extends InternalError {
+	name = `AuthenticationRequiredError`;
+
+	/**
+	 * The resource being accessed
+	 * @type {String|Number}
+	 */
+	entity; 
+
+	/**
+	 * @constructor
+	 * @param {String|Number} id - The resource being accessed
+	 * @param {String} message - any message
+	 */
+	constructor(id, message) {
+		super(message, 401);
+		this.entity = id;
+	};
+};
+
+/**
+ * When authentication fails
+ * @class AuthenticationError
+ * @extends AuthenticationRequiredError
+ */
+class AuthenticationError extends AuthenticationRequiredError {
+	name = `AuthenticationError`;
+
+	/**
+	 * @constructor
+	 * @param {String|Number} id - The resource being accessed
+	 * @param {String} message - any message
+	 */
+	constructor(id, message) {
+		super(id, message);
+	};
+};
+
+module.exports = {
+	AuthenticationError,
+	AuthenticationRequiredError
+};
